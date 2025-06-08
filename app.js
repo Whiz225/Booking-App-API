4547c3925edaa1e0cfbafd169caac577eb921508
Whiz225 <nwosuemmanuel1994@gmail.com>
2025-06-08 10:57:39 +0100

    app.js modified

--- app.js
+++ app.js
@@ -23,7 +23,9 @@ const app = express();
 
 //Enable CORS for all origins (during development)
 // access to only my frontend
-app.use(cors({ origin: 'http://localhost:5174', credentials: true }));
+app.use(
+  cors({ origin: ['http://localhost:5173/Booking-App/','https://booking-app-v2.onrender.com'], credentials: true }),
+);
 // access to all
 // app.use(cors())
 
